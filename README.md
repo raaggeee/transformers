9fdb2fadd964f20250613520b4756812e8750cd1
raaggeee <raaggee08@gmail.com>
2026-02-21 17:18:13 +0530

    implemented multi head attention

--- README.md
+++ README.md
@@ -18,3 +18,5 @@
 - [] Cross Encoding
 - [] Feed Forward Nerual Network
 - [] Add and Normalize
+
+## Reference https://medium.com/data-science/a-complete-guide-to-write-your-own-transformers-29e23f371ddd
---
+++ encoder/multi_head_attention.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, hidden_dims = 6, heads=3):
+        super().__init__()
+        self.Wq = nn.Linear(hidden_dims, hidden_dims, bias=False)
+        self.Wv = nn.Linear(hidden_dims, hidden_dims, bias=False)
+        self.Wk = nn.Linear(hidden_dims, hidden_dims, bias=False)
+        self.Wo = nn.Linear(hidden_dims, hidden_dims, bias=False)
+        assert hidden_dims % heads == 0
+        self.hidden_dims = hidden_dims
+        self.heads = heads
+        self.d_k = hidden_dims//heads
+
+    def scaled_dot_product_attention(self, q, k, v, mask):
+        dims_k = k.shape[-1]
+
+        # attention = torch.matmul(q, k.transpose(-2, -1))
+        attention = (q @ k.transpose(-2, -1)/math.sqrt(dims_k))
+
+        if mask is not None:
+            fill_val = -float("inf")
+            attention.masked_fill_(mask==0, fill_val)
+            
+        scaled_qk = F.softmax(attention)
+
+        scaled_attention = torch.matmul(scaled_qk, v)
+        
+        return scaled_attention
+    
+    def forward(self, q, k, v, mask=False): #this is also done for cross attention
+        q = self.Wq(q)
+        k = self.Wk(k)
+        v = self.Wv(v)
+        print(q.shape)
+
+        q = q.view(q.shape[0], q.shape[1], self.heads, self.d_k).transpose(1, 2) 
+        k = k.view(k.shape[0], k.shape[1], self.heads, self.d_k).transpose(1, 2)
+        v = v.view(v.shape[0], v.shape[1], self.heads, self.d_k).transpose(1, 2)
+        
+        x = self.scaled_dot_product_attention(q, k, v, mask)
+
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.heads*self.d_k)
+
+        return self.Wo(x)
