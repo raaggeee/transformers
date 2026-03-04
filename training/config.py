2b063f978323d3bffacdfc171a11251da80ecc93
raaggeee <raaggee08@gmail.com>
2026-03-05 01:17:56 +0530

    model weights and config files

--- training/config.py
+++ training/config.py
@@ -15,3 +15,23 @@ def get_config():
         "tokenizer_file": "",
         "exp_name": ""
     }
+
+def get_weights_file_path(config, epochs: str):
+    model_folder = f"{config["datasource"]}_{config["model_folder"]}"
+    model_filename = f"{config["model_basename"]}{epochs}.pt"
+    return str(Path(".")/model_folder/model_filename)
+
+def latest_weights(config):
+    model_folder = f"{config["datasource"]}_{config["model_folder"]}"
+    model_filename = f"{config["model_basename"]}*"
+
+    weights_files = list(Path(model_folder).glob(model_filename))
+
+    if len(weights_files) == 0:
+        return None
+
+    weights_files.sort()
+    return str(weights_files[-1])
+
+
+
